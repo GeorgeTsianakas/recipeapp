9a6fe437a47f7b48987a5152f52c4b92ae149a0f
George <gtsianakas94@gmail.com>
2020-03-16 17:26:18 +0200

    SpringDataJPA Commit
    Spring Data JPA Query methods

--- src/main/java/com/app/recipe/controllers/IndexController.java
+++ src/main/java/com/app/recipe/controllers/IndexController.java
@@ -1,13 +1,32 @@
 package com.app.recipe.controllers;
 
+import com.app.recipe.domain.Category;
+import com.app.recipe.domain.UnitOfMeasure;
+import com.app.recipe.repositories.CategoryRepository;
+import com.app.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        //System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        //System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 
--- src/main/java/com/app/recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/app/recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package com.app.recipe.repositories;
 import com.app.recipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+
 }
