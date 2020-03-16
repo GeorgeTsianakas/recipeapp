374375d6c53e040f531e39dc625f52b2fa8c1ff0
George <gtsianakas94@gmail.com>
2020-03-16 17:10:41 +0200

    Domain Commit v3
    JPA Data Modeling
    Many-to-Many JPA Relationships

--- src/main/java/com/app/recipe/domain/Ingredient.java
+++ src/main/java/com/app/recipe/domain/Ingredient.java
@@ -12,6 +12,9 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
     @ManyToOne
     private Recipe recipe;
 
@@ -45,6 +48,14 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 
 }
